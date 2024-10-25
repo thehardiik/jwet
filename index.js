780efe257f13b831ebb285f34f1594ed5dcb01f6
Hardik Srivastav <hardiknjms21@gmail.com>
2024-10-25 17:38:56 +0530

    Added Encryption

--- index.js
+++ index.js
@@ -12,18 +12,24 @@ const payload = {
 
 const secretKey = "abcde";
 const token = "eyJhbGciOiJIUzI1NiIsInR5cGUiOiJKV1QifQ.eyJuYW1lIjoiSGFyZGlrIFNyaXZhc3RhdiIsImRldmljZSI6IlJlYWxtZUMzIn0.YTJiMTQwN2I3ZWQzOWNlYzZmODIxNjVjOTAyOGU3YzU5MWFiOWE4ZDU4YmM5YzllODU2ODZmZGM0NTA1YjIwNQ"
-//console.log(createToken(payload, secretKey))
+console.log(createToken(payload, secretKey, true))
 //console.log(verifyToken(token , secretKey))
 //console.log(base64encoding(header))
 
-function createToken(payload, secretKey){
+function createToken(payload, secretKey, toEncrypt = false){
     const header = {
         alg: "HS256",
         type: "JWT"
     }
     
     const encodedHeader = base64urlencoding(Buffer.from(JSON.stringify(header)))
-    const encodedPayload = base64urlencoding(Buffer.from(JSON.stringify(payload)))
+    let encodedPayload = base64urlencoding(Buffer.from(JSON.stringify(payload)))
+
+    if(toEncrypt){
+        const hmacPayload = crypto.createHmac('sha256', secretKey).update(encodedPayload)
+        const hmacDigestPayload = hmacPayload.digest('hex');
+        encodedPayload = base64urlencoding(Buffer.from(hmacDigestPayload))
+    }
 
     const hmac = crypto.createHmac('sha256', secretKey).update(encodedHeader + '.' + encodedPayload)
     const hmacDigest = hmac.digest('hex');
